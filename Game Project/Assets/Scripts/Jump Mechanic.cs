a074ba5a5911a88a159e943d0314650e93ebead7
Earthan Huq <ehuq000@citymail.cuny.edu>
2025-10-17 15:14:14 -0400

    0.0.012
    Fixed jumping more and betterer.
    Ripped out almost all of the logic at every level of the jump mechanic and replaced it with more efficient modular solutions.

--- Game Project/Assets/Scripts/Jump Mechanic.cs
+++ Game Project/Assets/Scripts/Jump Mechanic.cs
@@ -1,58 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Mechanics
 {
     public class JumpMechanic : MonoBehaviour
     {
-        public float jumpForce = 7f; // You can change this in the Inspector
+        public float jumpForce = 7f;
         Rigidbody2D rb;
-        bool isGrounded;
-        bool canDoubleJump;
-
+        int jumpCounter;            // Counts how many times the player has jumped.
+        public int maxJumps = 1;           // The number of times the player can jump before touching the ground again.
+        Vector2 position;           // To store the position of the player.
+        Vector2 direction = Vector2.down; // Part of the ground-check raycast maneuver below.
+        public float distance;  // This is adjustable to accomodate future changes to the player sprite.
+        private LayerMask groundLayer;
+        private RaycastHit2D groundSensor; // A ray fired from 'position' straight down at a 'distance' of just below the player's feet.
+        
         void Start()
         {
-            rb = GetComponent<Rigidbody2D>();
+            rb = GetComponent<Rigidbody2D>();  // This object's RigidBody2D component, for short.
+            groundLayer = LayerMask.GetMask("Ground");
         }
 
         void Update()
         {
+            position = transform.position;      // Keep 'position' updated with the player's present position.
+            
             // Jump input
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && jumpCounter < maxJumps)  // If the player presses 'Space' AND the current jump counter does not exceed the maximum...
             {
-                if (isGrounded)
-                {
-                    // Regular jump
-                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                    canDoubleJump = true; // Allow double jump after the first jump
-                }
-                else if (canDoubleJump)
-                {
-                    // Double jump
-                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                    canDoubleJump = false; // Consume double jump
-                }
+                jumpCounter++;  // Increment the counter by 1 each time the player jumps, then
+                print("jumpCounter = " +  jumpCounter);
+                rb.linearVelocityY = 0; // reset vertical velocity so each jump is equally strong, and finally
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);  // add impulse force to the character's positive Y axis, according to the current jump force.
             }
-        }
-
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            // Check if player landed on ground
-            if (collision.gameObject.CompareTag("Ground"))      //This is giving True when touching any part of platform, including the bottom.
+            
+            
+            // / Ground Check Logic:
+            // - The RaycastHit2D variable named 'groundSensor' will now be supplied the hit data from a 2D ray.
+            //      - groundSensor was declared at the above with the other variables, but not defined. It will now be defined, below:
+            // - The ray will originate at the player's 'position'. (transform.position)
+            // - The 'direction' will be straight down. (Vector2.down)
+            // - The 'distance' the ray will travel will terminate it at a point just below the player's feet. (adjustible in the Unity Editor)
+            // - The ray will exist only on the "Ground" layer. Thereby, it can only "hit" objects that are also on that layer.
+            groundSensor = Physics2D.Raycast(position, direction, distance, groundLayer);
+            
+            if (groundSensor)  // the 'groundSensor' (a RaycastHit2D) will return "true" if it contains any information at all, and "null" if it doesn't.
             {
-                isGrounded = true;
-                canDoubleJump = false;
+                print("There's the ground!");
+                jumpCounter = 0;  // Restore the player's ability to jump, because their feet have touched the ground.
             }
+            Debug.DrawLine(position, new Vector2(position.x, position.y - distance), Color.red);  //Visualize the sensor ray in the editor for calibration.
+            
         }
-
-        void OnCollisionExit2D(Collision2D collision)       //Not sure if this is how we wanna do this. Might be causing problems. -E
-        {
-            // Check if player left the ground
-            if (collision.gameObject.CompareTag("Ground"))
-            {
-                isGrounded = false;
-            }
-        }
-
-
     }
 }
